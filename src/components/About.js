1c3a3fb4ea5dc434406968088e9b2beeab1fd12e
Liam <liam.healy1@marist.edu>
2020-01-09 12:08:19 -0500

    Re-styled everything

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+export default function Projects() {
+
+	return (
+		<Container fluid>
+            <h3 style={{color: 'white', margin: 30}}>
+                Experienced in Ruby, JavaScript and Java based programming and a 
+                Bachelor’s Degree in Computer Science. My interest in modifying video 
+                games led me to learn more about software development and design, 
+                which combined with my strategic and project management skills 
+                will help tech companies develop and improve their applications.
+            </h3>
+		</Container>
+	)
+}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Grid, Segment, Embed, Container } from 'semantic-ui-react';
+import { Grid, Image, Container } from 'semantic-ui-react';
+import { Icon } from '@iconify/react';
+import youtubeIcon from '@iconify/icons-simple-icons/youtube';
+import twitch from '../assets/images/twitch.png';
+import rezzemay from '../assets/images/rezzemay.png';
+import groupit from '../assets/images/groupit.png';
 
 export default function Projects() {
 	return (
@@ -13,71 +18,30 @@ export default function Projects() {
 				alignItems: 'center',
 				justifyContent: 'center',
 				backgroundImage: 'linear-gradient(-315deg, rgb(24, 20, 43) 0%, rgb(14, 34, 43) 51%, rgb(12, 51, 46) 100%)'
-				// backgroundColor: 'white'
-			}}
-		>
-			<Grid container divided="vertically">
-				<Grid.Row columns={2}>
+			}}>
+			<h2 style={{color: 'white'}}>Check out some of my most recent work.</h2>
+			<Grid container relaxed="very">
+				<Grid.Row columns={3}>
 					<Grid.Column>
-						<div className="Grid-item-small">
-							<Embed
-								id='1'
-								title='Flatiron // Twitch'
-								source='youtube'
-								iframe={{
-									title: "Flatiron // Twitch",
-									width: "560",
-									height: "315", 
-									src: "https://www.youtube.com/embed/6LJpu5la6Tk",
-									frameBorder: "0",
-									allow: "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture",
-								}}
-							/>
-						</div>
+						<Image src={twitch} size="large" rounded />
+						<h3 style={{color: 'white'}}>Twitch Dashboard & Forum</h3>
+						<a href="https://github.com/liamhealy" target="_blank" rel="noopener noreferrer">
+                <Icon icon={youtubeIcon} height='2em'/>
+            </a>
 					</Grid.Column>
 					<Grid.Column>
-						<Segment placeholder style={{height: '100%', backgroundColor: '#c3cee1'}}>
-							<h4>
-								This project is a Twitch dashboard and forum. This application was 
-								built using React and React-router along with 
-								material-ui on the frontend, while on the backend I built a JSON 
-								API using Ruby on Rails. In addition, I incorporated the new Twitch
-								API and a few handy npm packages.
-							</h4>
-						</Segment>
-					</Grid.Column>
-				</Grid.Row>
-				<Grid.Row columns={2}>
-					<Grid.Column>
-						<div className="Grid-item-small">
-							<Embed
-								id='1'
-								title='Flatiron // Twitch'
-								source='youtube'
-								iframe={{
-									title: "Flatiron // Twitch",
-									width: "560",
-									height: "315", 
-									src: "https://www.youtube.com/embed/--RuWbBQsaY",
-									frameBorder: "0",
-									allow: "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture",
-								}}
-							/>
-						</div>
+						<Image src={rezzemay} size="large" rounded />
+						<h3 style={{color: 'white'}}>Rezzemay</h3>
+						<a href="https://github.com/liamhealy" target="_blank" rel="noopener noreferrer">
+                <Icon icon={youtubeIcon} height='2em'/>
+            </a>
 					</Grid.Column>
 					<Grid.Column>
-						<Segment placeholder style={{
-							height: '100%', 
-							backgroundImage: 'linear-gradient(-225deg, rgb(24, 20, 43) 0%, rgb(14, 34, 43) 51%, rgb(12, 51, 46) 100%)',
-							color: 'white'
-						}}>
-							<h4>
-								This application is a dynamic portfolio generator for software
-								developers. I built this application using React and React-router
-								with semantic-ui on the frontend. For the backend I built a JSON API
-								using Ruby on Rails.
-							</h4>
-						</Segment>
+						<Image src={groupit} size="large" rounded />
+						<h3 style={{color: 'white'}}>Group-It</h3>
+						<a href="https://github.com/liamhealy" target="_blank" rel="noopener noreferrer">
+                <Icon icon={youtubeIcon} height='2em'/>
+            </a>
 					</Grid.Column>
 				</Grid.Row>
 			</Grid>
