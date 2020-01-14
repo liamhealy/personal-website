3231dabccd019773e8b153bb54fa52cd1a6298b6
Liam <liam.healy1@marist.edu>
2020-01-13 19:06:19 -0500

    Style changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,27 +19,34 @@ export default function Projects() {
 				justifyContent: 'center',
 				backgroundImage: 'linear-gradient(-315deg, rgb(24, 20, 43) 0%, rgb(14, 34, 43) 51%, rgb(12, 51, 46) 100%)'
 			}}>
-			<h2 style={{color: 'white'}}>Check out some of my most recent work.</h2>
+			<h2 style={{color: 'white', fontWeight: 'bolder'}}>What I've been up to</h2>
+			<h3 style={{color: 'white', marginBottom: 30}}>
+				Graduated from the Flatiron School's software engineering program
+				in January of 2020, and got my B.S. in Computer Science from Marist
+				College in May of 2019. Continuing work on some cool side projects
+				but primarily seeking a full-time full stack development position.
+			</h3>
+			<h2 style={{color: 'white', fontWeight: 'bolder'}}>Check out some of my most recent work.</h2>
 			<Grid container relaxed="very">
 				<Grid.Row columns={3}>
 					<Grid.Column>
 						<Image src={twitch} size="large" rounded />
 						<h3 style={{color: 'white'}}>Twitch Dashboard & Forum</h3>
-						<a href="https://github.com/liamhealy" target="_blank" rel="noopener noreferrer">
+						<a href="https://www.youtube.com/watch?v=k49eaST9eJg&t=181s" target="_blank" rel="noopener noreferrer">
                 <Icon icon={youtubeIcon} height='2em'/>
             </a>
 					</Grid.Column>
 					<Grid.Column>
 						<Image src={rezzemay} size="large" rounded />
 						<h3 style={{color: 'white'}}>Rezzemay</h3>
-						<a href="https://github.com/liamhealy" target="_blank" rel="noopener noreferrer">
+						<a href="https://www.youtube.com/watch?v=H5TJhYSPwb4&t=72s" target="_blank" rel="noopener noreferrer">
                 <Icon icon={youtubeIcon} height='2em'/>
             </a>
 					</Grid.Column>
 					<Grid.Column>
 						<Image src={groupit} size="large" rounded />
 						<h3 style={{color: 'white'}}>Group-It</h3>
-						<a href="https://github.com/liamhealy" target="_blank" rel="noopener noreferrer">
+						<a href="https://www.youtube.com/watch?v=U1KvPoPBTiI" target="_blank" rel="noopener noreferrer">
                 <Icon icon={youtubeIcon} height='2em'/>
             </a>
 					</Grid.Column>
